8dd867db6021abbda42447c7a59ec35dbf4d34d9
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-01-12 17:06:52 -0500

    Add :: About.css

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Info = () => {
     return (
-        <div>
-            
+        <div className="about_info grid">
+            <div className="about_box">
+                <i className='bx bx-award about_icon'></i>
+                <h3 className="about_title">Experience</h3>
+                <apan className="about_subtitle">8 Years Working</apan>
+            </div>
+
+            <div className="about_box">
+                <i className='bx bx-briefcase-alt about_icon'></i>
+                <h3 className="about_title">Completed</h3>
+                <apan className="about_subtitle">48 + Projects</apan>
+            </div>
+
+            <div className="about_box">
+                <i className='bx bx-support about_icon'></i>
+                <h3 className="about_title">Support</h3>
+                <apan className="about_subtitle">Online 24/7</apan>
+            </div>
         </div>
     );
 }
