bf068540755b60878792d06cdfccb8ab362708cb
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-01-30 15:16:17 -0500

    Change :: Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,58 +1,53 @@
-import React, { useState } from 'react'
-import './header.css'
+import React, { useState } from "react";
+import "./header.css";
 
 const Header = () => {
-    /*=========================== TOGGLE MENU ===========================*/
-    const [Toggle, showMenu] = useState(false)
+  /*=========================== TOGGLE MENU ===========================*/
+  const [Toggle, showMenu] = useState(false);
 
-    return (
-        <>
-            <header className='header'>
-                <nav className="nav container">
-                    <a href="index.html" className="nav_logo">Barros</a>
+  return (
+    <>
+      <header className="header">
+        <nav className="nav container">
+          <a href="index.html" className="nav_logo">
+            Barros
+          </a>
 
-                    <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
-                        <ul className="nav_list grid">
-                            <il className="nav_item">
-                                <a href="#home" className="nav_link active_link">
-                                    <i className="uil uil-estate nav_icon"></i> Home
-                                </a>
-                            </il>
-                            <il className="nav_item">
-                                <a href="#about" className="nav_link">
-                                    <i className="uil uil-user nav_icon"></i> About
-                                </a>
-                            </il>
-                            <il className="nav_item">
-                                <a href="#skills" className="nav_link">
-                                    <i className="uil uil-file-alt nav_icon"></i> Skills
-                                </a>
-                            </il>
-                            <il className="nav_item">
-                                <a href="#services" className="nav_link">
-                                    <i className="uil uil-briefcase-alt nav_icon"></i> Services
-                                </a>
-                            </il>
-                            <il className="nav_item">
-                                <a href="#portfolio" className="nav_link">
-                                    <i className="uil uil-scenery nav_icon"></i> Portfolio
-                                </a>
-                            </il>
-                            <il className="nav_item">
-                                <a href="#contact" className="nav_link">
-                                    <i className="uil uil-message nav_icon"></i> Contact
-                                </a>
-                            </il>
-                        </ul>
-                        <il className="uil uil-times nav_close" onClick={() => showMenu(!Toggle)}></il>
-                    </div>
-                    <div className="nav_toggle" onClick={() => showMenu(!Toggle)}>
-                        <il className="uil uil-apps"></il>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
-}
+          <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
+            <ul className="nav_list grid">
+              <il className="nav_item">
+                <a href="#portfolio" className="nav_link">
+                  <i className="uil uil-scenery nav_icon"></i> Inicio
+                </a>
+              </il>
+              <il className="nav_item">
+                <a href="#portfolio" className="nav_link">
+                  <i className="uil uil-scenery nav_icon"></i> Portafolio
+                </a>
+              </il>
+              <il className="nav_item">
+                <a href="https://medium.com/@barrosjss" target="_blank" className="nav_link">
+                  <i className="uil uil-scenery nav_icon"></i> Blog
+                </a>
+              </il>
+              <il className="nav_item">
+                <a href="#contact" className="nav_link">
+                  <i className="uil uil-message nav_icon"></i> Contacto
+                </a>
+              </il>
+            </ul>
+            <il
+              className="uil uil-times nav_close"
+              onClick={() => showMenu(!Toggle)}
+            ></il>
+          </div>
+          <div className="nav_toggle" onClick={() => showMenu(!Toggle)}>
+            <il className="uil uil-apps"></il>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
